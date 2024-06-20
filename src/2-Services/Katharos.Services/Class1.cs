9e42020292698977014b09f7f73be2ba1cce0470
Wesley Ford <wes@wesdevpro.com>
2024-06-20 18:05:29 -0400

    chore: convert all class libs to Maui class lib
    1. Updated class libraries to Maui libraries.
    2. Made xUnit projects Maui compatible (Source: https://github.com/xunit/xunit/discussions/2732#discussioncomment-6238237)
       - <PackageReference Include="Microsoft.Maui.Controls" Version="8.0.40" /> (Recommended by the dotnet compiler)
    3. Updated all Maui `.csproj(s)` to only build for windows (this is temporary)

--- src/2-Services/Katharos.Services/Class1.cs
+++ src/2-Services/Katharos.Services/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace Katharos.Models;
+﻿namespace Katharos.Services;
 
 // All the code in this file is included in all platforms.
 public class Class1
--- src/2-Services/Katharos.Services/Interfaces/IAccountService.cs
+++
@@ -1,9 +0,0 @@
-namespace Katharos.Services.Interfaces
-{
-   public interface IAccountService
-   {
-      void Add();
-      void Remove();
-      void Update();
-   }
-}
